e6901977358a335850874395f51ea3ae5421ba4d
youngjaekwon <gczzga@gmail.com>
2025-06-24 23:22:14 +0900

    refactor: remove jwt authentication
    - default authentication classes 에서 jwt authentication을 제거
    - 이는 post의 작성/수정/삭제는 django admin에서만 가능하고,
    - 댓글 부분은 로그인을 하지 않아도 작성 가능하기 때문

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -143,9 +143,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # REST Framework 설정
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
